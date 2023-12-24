445c8db163326078789a5f860956812e617974cc
Hassaan Ahmed <codinghassaan@gmail.com>
2023-12-24 23:14:57 +0500

    reviews-req

---
+++ src/app/(adminRoot)/(admin)/reviews-request/page.tsx
@@ -0,0 +1,28 @@
+import { ReviewRequest } from "@/components";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="bg-white rounded-xl">
+      <div className="flex  border-b border-gray-200 w-full gap-5">
+        <div className="flex p-4  gap-5">
+          <p className="text-[#EC583C]">Products</p>
+          <p className="text-[#252530]/50">Vendors</p>
+        </div>
+      </div>
+      <div className="pt-5">
+        <div className="p-5 flex flex-col gap-5">
+          <ReviewRequest />
+          <ReviewRequest />
+          <ReviewRequest />
+          <ReviewRequest />
+          <ReviewRequest />
+          <ReviewRequest />
+          <ReviewRequest />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
